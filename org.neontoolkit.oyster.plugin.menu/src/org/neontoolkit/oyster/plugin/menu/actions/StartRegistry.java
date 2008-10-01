bcc70798ba0cb702d3b45a259dd20b8b4d89458f
rpa <rpa@d3cfc1af-d561-4ec3-8170-ff15b61e6928>
2008-10-01 00:43:26 +0000

    changes :):) final

--- org.neontoolkit.oyster.plugin.menu/src/org/neontoolkit/oyster/plugin/menu/actions/StartRegistry.java
+++ org.neontoolkit.oyster.plugin.menu/src/org/neontoolkit/oyster/plugin/menu/actions/StartRegistry.java
@@ -13,8 +13,9 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.neontoolkit.registry.api.Oyster2Connection;
 import org.neontoolkit.registry.api.Oyster2Manager;
+import org.neontoolkit.registry.oyster2.Constants;
+import org.neontoolkit.registry.oyster2.Properties;
 import org.neontoolkit.registry.util.EntryDetailSerializer;
-
 import com.ontoprise.ontostudio.gui.GuiPlugin;
 
 /**
@@ -36,13 +37,15 @@ public class StartRegistry implements IWorkbenchWindowActionDelegate {
 	private IPreferenceStore _store = GuiPlugin.getDefault().getPreferenceStore();
 	private String sOyster = "";
 	private String pOyster = "";
+	private Boolean readLocally=false;
+	private IAction actionNew;
 	
 	
 	/**
 	 * The constructor.
 	 */
 	public StartRegistry() {
-		
+
 	}
 
 	public static boolean getState(){
@@ -56,6 +59,7 @@ public class StartRegistry implements IWorkbenchWindowActionDelegate {
 	 * @see IWorkbenchWindowActionDelegate#run
 	 */
 	public void run(IAction action) {
+		actionNew=action;
 		if (start){			
 			Job exportJob = new Job("Starting registry...(The first time please wait few minutes)") {
 				protected IStatus run(IProgressMonitor monitor) {
@@ -65,15 +69,16 @@ public class StartRegistry implements IWorkbenchWindowActionDelegate {
 						monitor.worked(10);
 						sOyster = _store.getString("SUPEROYSTER");
 						pOyster = _store.getString("PUSHOYSTER");
+						readLocally=_store.getBoolean("READLOCALLY");
+						if (readLocally) setOntologies();
+							
 						if(serverProcess==null && !superOysterStorage()) {
 							System.out.println("executing the server...");
 							serverProcess = Runtime.getRuntime().exec("java -cp "+ EntryDetailSerializer.QUOTE + o2File + EntryDetailSerializer.QUOTE + " org.neontoolkit.registry.oyster2.server.StartServer -workflowSupport -startKAON2 -noAutomaticSync");
 						}
 						if(serverProcess==null && !superOysterStorage()){
-							MessageDialog.openInformation(
-									window.getShell(),
-									"Menu Plug-in",
-									"Registry couldn't start");
+							openMessage("Registry couldn't start");
+							changeAction();
 						}
 						else{	
 							monitor.worked(20);
@@ -81,31 +86,32 @@ public class StartRegistry implements IWorkbenchWindowActionDelegate {
 								File tFile = new File("O2serverfiles/localRegistry.owl"); //FIXED FOR NOW...
 								if ((!tFile.exists())
 										|| (tFile.length() <= 0)) {
-									System.out.println("waiting 30S...");
-									Thread.sleep(30000);
+									if (!readLocally){
+										System.out.println("waiting 30S...");
+										Thread.sleep(30000);
+									}else Thread.sleep(5000);
 								}
-								else Thread.sleep(7000);
+								else {
+									if (!readLocally)
+										Thread.sleep(7000);
+									else
+										Thread.sleep(1000);
+								}
 							}
 							monitor.worked(30);
 							startOysterSilent();
 							if (!success) {
-								MessageDialog.openInformation(
-										window.getShell(),
-										"Menu Plug-in",
-										"Registry couldn't start");
-								
+								openMessage("Registry couldn't start");
+								changeAction();
 							}else{
 								monitor.worked(40);
 								start = !start;
+								changeAction();
 							}
 						}
 					}catch (Exception e) {
-						// TODO Auto-generated catch block
-						MessageDialog.openInformation(
-								window.getShell(),
-								"Menu Plug-in",
-								"Registry couldn't start");
-						
+						openMessage("Registry couldn't start");
+						changeAction();
 						e.printStackTrace();
 					}	
 					finally{
@@ -118,23 +124,24 @@ public class StartRegistry implements IWorkbenchWindowActionDelegate {
 			exportJob.schedule();
 		}
 		else {
-			stopOyster();
-			serverProcess.destroy();
-			serverProcess = null;
-			MessageDialog.openInformation(
-				window.getShell(),
-				"Menu Plug-in",
-				"Registry stopping...");
-			start = !start;
-		}
-		
-		if (start) {
-			action.setText(startRegistryText);
-			
-		}
-		else {
-			action.setText(stopRegistryText);
-			
+			boolean ans=
+				MessageDialog.openQuestion(
+						window.getShell(),
+						"Menu Plug-in",
+						"Are you sure you want to stop Oyster?");
+			if (ans){
+				stopOyster();
+				serverProcess.destroy();
+				try {
+					serverProcess.waitFor();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				serverProcess = null;
+				start = !start;
+				changeAction();
+			}
 		}
 	}
 
@@ -149,6 +156,17 @@ public class StartRegistry implements IWorkbenchWindowActionDelegate {
 		action.setEnabled(true);
 	}
 
+	public void changeAction(){
+		if (start) {
+			actionNew.setChecked(false);
+			actionNew.setText(startRegistryText);	
+		}
+		else {
+			actionNew.setChecked(true);
+			actionNew.setText(stopRegistryText);
+		}
+	}
+	
 	/**
 	 * We can use this method to dispose of any system
 	 * resources we previously allocated.
@@ -202,7 +220,6 @@ public class StartRegistry implements IWorkbenchWindowActionDelegate {
 	public void stopOyster(){
 		Job job = new Job("Closing import wizard session...") {
 			protected IStatus run(IProgressMonitor monitor) {
-					// Do some work
 					connection=null;
 					Oyster2Manager.closeConnection();
 					System.out.println("connection closed at the end..."); 
@@ -211,5 +228,31 @@ public class StartRegistry implements IWorkbenchWindowActionDelegate {
 			};
 		job.setUser(true);
 		job.schedule();
+	}
+	
+	public void openMessage(final String mess){
+		window.getShell().getDisplay().asyncExec(new Runnable() {
+	           public void run() {
+	        	   MessageDialog.openInformation(
+           				window.getShell(),
+           				"Menu Plug-in",
+           				mess);
+	            }
+		});
+	}
+	
+	public void setOntologies(){
+		Properties mprop = Oyster2Manager.getOysterProperties();
+		mprop.init();
+		mprop.setString(Constants.TypeOntology, "O2serverfiles/OMV.owl");
+		mprop.setString(Constants.PDOntology, "O2serverfiles/pOMV.owl");
+		mprop.setString(Constants.MDOntology, "O2serverfiles/mappingOMV.owl");
+		mprop.setString(Constants.owlChangeOntology, "O2serverfiles/OWLChanges.owl");
+		mprop.setString(Constants.TopicOntology, "O2serverfiles/dmozT.rdf");
+		mprop.setString(Constants.changeOntology, "O2serverfiles/changes.owl");
+		mprop.setString(Constants.owlodmOntology, "O2serverfiles/owl11v1.5.owl");
+		mprop.setString(Constants.workflowOntology, "O2serverfiles/workflow.owl");
+		mprop.setString(Constants.Image, "O2serverfiles/o1.gif");
+		mprop.storeOn();
 	}
 }
