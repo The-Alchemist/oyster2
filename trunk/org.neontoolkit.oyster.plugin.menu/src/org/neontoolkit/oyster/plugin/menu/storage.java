4320ae9c7f1a23dca5e6aedc878bd8d63b4939d3
rpa <rpa@d3cfc1af-d561-4ec3-8170-ff15b61e6928>
2008-10-01 00:43:26 +0000

    changes :):) final

--- trunk/org.neontoolkit.oyster.plugin.menu/src/org/neontoolkit/oyster/plugin/menu/storage.java
+++ trunk/org.neontoolkit.oyster.plugin.menu/src/org/neontoolkit/oyster/plugin/menu/storage.java
@@ -1,18 +1,29 @@
 package org.neontoolkit.oyster.plugin.menu;
 
+import java.io.File;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.neontoolkit.oyster.plugin.menu.actions.StartRegistry;
 import org.neontoolkit.registry.api.Oyster2Connection;
 
 import com.ontoprise.ontostudio.gui.GuiPlugin;
@@ -21,13 +32,16 @@ public class storage extends PreferencePage implements IWorkbenchPreferencePage 
 
 	public static final String SUPEROYSTER = "SUPEROYSTER";
 	public static final String PUSHOYSTER = "PUSHOYSTER";
+	public static final String READLOCALLY = "READLOCALLY";
 	private IPreferenceStore _store;
 	private Text superOysterIP;
 	private Text pushOysterIP;
 	private Group loginGroup;
 	Oyster2Connection oyster2Conn;
-	
-	
+	BooleanFieldEditor readLocally;
+	private Boolean isReadLocally;
+	private Button reset;
+	static Shell shell=null;
 	
 	public storage() {
 		super();
@@ -50,6 +64,7 @@ public class storage extends PreferencePage implements IWorkbenchPreferencePage 
 	protected Control createContents(Composite parent) {
 		// TODO Auto-generated method stub
 		
+		shell=this.getShell();
 		int columns = 1;
         Composite composite = new Composite(parent, SWT.NONE);
         GridLayout layout = new GridLayout(columns, false);
@@ -83,7 +98,60 @@ public class storage extends PreferencePage implements IWorkbenchPreferencePage 
         	pushOysterIP.setText(oldU);
         }
         
+        readLocally = new BooleanFieldEditor(READLOCALLY, "Read ontologies locally",
+				BooleanFieldEditor.SEPARATE_LABEL, loginGroup);
+        readLocally.setPreferenceStore(_store);
+        readLocally.load();
+        isReadLocally=_store.getBoolean(READLOCALLY);
+        readLocally.setPropertyChangeListener(new IPropertyChangeListener() {
+			public void propertyChange(PropertyChangeEvent event) {
+				isReadLocally=((Boolean) event.getNewValue()).booleanValue();
+			}
+		});
 		
+        reset = new Button(loginGroup, SWT.NONE);
+        reset.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				if (StartRegistry.connection==null && StartRegistry.serverProcess==null){
+					try{
+						File tFile = new File("O2serverfiles/localRegistry.owl");
+						if (tFile.exists())	tFile.delete();
+						tFile = new File("server/localRegistry.owl");
+						if (tFile.exists())	tFile.delete();
+						tFile = new File(".propertiesOyster");
+						if (tFile.exists())	tFile.delete();
+					}catch(Exception ex){
+						MessageDialog.openInformation(
+		           				shell,
+		           				"Menu Plug-in",
+		           				"Reset failed...");
+					}
+					finally{
+						MessageDialog.openInformation(
+		           				shell,
+		           				"Menu Plug-in",
+		           				"Oyster has been reset (Its recommended to restart the NTK)...");
+					}
+				}else if ((StartRegistry.connection!=null && StartRegistry.serverProcess==null) || 
+						(StartRegistry.connection==null && StartRegistry.serverProcess!=null)){
+					MessageDialog.openInformation(
+	           				shell,
+	           				"Menu Plug-in",
+	           				"Cannot reset Oyster. Please restart the NTK and reset again.");
+				}
+				else{ 
+					MessageDialog.openInformation(
+	           				shell,
+	           				"Menu Plug-in",
+	           				"Cannot reset Oyster. Please first shutdown Oyster...");
+				}
+			}
+		});
+		final GridData gd_button = new GridData(SWT.FILL, SWT.CENTER, false, false);
+		gd_button.widthHint = 80;
+		reset.setLayoutData(gd_button);
+		reset.setText("Reset Oyster");
+        
 		return composite;
 	}
 
@@ -95,6 +163,7 @@ public class storage extends PreferencePage implements IWorkbenchPreferencePage 
 	protected void performApply() {      
 		_store.setValue(SUPEROYSTER, superOysterIP.getText().trim());
 		_store.setValue(PUSHOYSTER, pushOysterIP.getText().trim());
+		_store.setValue(READLOCALLY, isReadLocally);
     }
 
     @Override
@@ -104,9 +173,11 @@ public class storage extends PreferencePage implements IWorkbenchPreferencePage 
     }
     
     protected void performDefaults() {
-    	superOysterIP.setText("");
-    	pushOysterIP.setText("");
     	_store.setValue(SUPEROYSTER, "");
     	_store.setValue(PUSHOYSTER, "");
+    	_store.setValue(READLOCALLY, false);
+    	superOysterIP.setText("");
+    	pushOysterIP.setText("");
+    	readLocally.load();
     }
 }
