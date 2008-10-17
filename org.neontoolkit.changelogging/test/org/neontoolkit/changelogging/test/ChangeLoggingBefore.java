3c58d5c2cd794f0246543a9c660c49c8386dee5d
rpa <rpa@d3cfc1af-d561-4ec3-8170-ff15b61e6928>
2008-10-17 18:45:34 +0000

    Improving JUNIT tests

--- org.neontoolkit.changelogging/test/org/neontoolkit/changelogging/test/ChangeLoggingBefore.java
+++ org.neontoolkit.changelogging/test/org/neontoolkit/changelogging/test/ChangeLoggingBefore.java
@@ -1,8 +1,5 @@
 package org.neontoolkit.changelogging.test;
 
-
-
-import java.io.File;
 import java.net.URI;
 import java.util.Properties;
 import org.eclipse.core.resources.IProject;
@@ -13,7 +10,6 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import com.ontoprise.ontostudio.io.ImportExportControl;
 import com.ontoprise.config.IConfig;
@@ -25,7 +21,7 @@ import com.ontoprise.ontostudio.owl.perspectives.OWLPerspective;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 
-public class ChangeLoggingAbstractTest {
+public class ChangeLoggingBefore {
 
 	protected static IWorkbench _workbench;
     protected static MTreeView _schemaView;
@@ -37,7 +33,7 @@ public class ChangeLoggingAbstractTest {
     protected static final String ONTOLOGY_PROJECT = "testproject"; 
 	
 	@BeforeClass 
-	   public static void runBeforeClass() throws Exception {
+	public static void runBeforeClass() throws Exception {
 		prepareViews();
 		prepareWorkspace();
 		assertEquals("Workspace is not empty", 0, ResourcesPlugin.getWorkspace().getRoot().getProjects().length);
@@ -45,16 +41,6 @@ public class ChangeLoggingAbstractTest {
 		loadOntology();
 	}
 	
-	@AfterClass 
-	   public static void runAfterClass() throws Exception {
-		File tFile = new File("server/localRegistry.owl");
-		if (tFile.exists())	tFile.delete();
-		tFile = new File(".propertiesOyster");
-		if (tFile.exists())	tFile.delete();
-		tFile = new File("server");
-		if (tFile.exists())	tFile.delete();
-		prepareWorkspace();
-	}
 	
 	protected static void prepareViews() throws Exception {
         _workbench = PlatformUI.getWorkbench();
@@ -67,7 +53,7 @@ public class ChangeLoggingAbstractTest {
     }
 
 	
-	protected static void prepareWorkspace() throws Exception {
+	public static void prepareWorkspace() throws Exception {
 	    String[] projects = DatamodelPlugin.getDefault().getOntologyProjects();
 	    for (int i = 0; i < projects.length; i++) {
 	        String[] ontologies = DatamodelPlugin.getDefault().getProjectOntologies(projects[i]);
